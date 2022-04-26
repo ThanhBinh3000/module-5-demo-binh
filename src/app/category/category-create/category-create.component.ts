953b08ffea7436290af6af092a15b148bc8d8e72
daothannhangian <binhk4a@ gmail.com>
2022-04-26 22:13:21 +0700

    tl

---
+++ src/app/category/category-create/category-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {CategoryService} from '../../service/category.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-category-create',
+  templateUrl: './category-create.component.html',
+  styleUrls: ['./category-create.component.css']
+})
+export class CategoryCreateComponent implements OnInit {
+  categoryForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
+  });
+  constructor(private categoryService: CategoryService, private router: Router) { }
+  submit() {
+    const category = this.categoryForm.value;
+    this.categoryService.saveCategory(category);
+    this.categoryForm.reset();
+    this.router.navigate(['/category/list']);
+  }
+  ngOnInit() {
+  }
+
+}
